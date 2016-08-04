1b7909605b035977fcb56a95925621b77befb32f
u1and0 <e01.ando60@gmail.com>
2016-08-04 17:45:49 +0900

    insの値を変えることで、返されるリストがinsずつ増えた値を穴埋め

--- filler.py
+++ filler.py
@@ -1,23 +1,29 @@
+# coding: utf-8
 from itertools import *
 from more_itertools import *
+import datetime
+d=datetime
 
-def pair(x):
+def fill(li,ins):
 	'''
-	引数x(リスト形式)を二つずつのペアに分け、fill関数に渡す
+	引数:
+		li:リスト
+		ins:int
+	戻り値：編集を加えた、引数と同じリスト
 	'''
-	for two in list(pairwise(x)):   #2つずつにわける
+	for two in list(pairwise(li)):   #liの中身を2つずつにわける
 		print(two)
-		if two[-1]-two[0]>1:
-			for i in range(two[0]+1,two[-1]):   #rangeの逆順コピーを取り出す
-				x.insert(x.index(two[-1]),i)
+		if two[-1]-two[0]>ins:   #抜き出したタプルの要素の差がins上であれば
+			for i in range(two[0]+ins,two[-1],ins):
+				li.insert(li.index(two[-1]),i)   #タプルの要素間の場所にinsずつ増やした値を入れる
 				print('insert',i)
 		else:print('OK!')
-	return x
+	return li
 
 
 
 '''TEST'''
-li=[[1,5,6],[1,3],[0,1,8,11,16],[1,6,8,9,10,11,15]]
+li=[[1,50],[0,8,10,16],[1,5,9,11,14,15]]
 for x in li:
-	print(pair(x))
-	# print(bool(pair(x)==list(range(x[0],x[-1]+1))))
+	print(fill(x,10))
+	# print(bool(fill(x)==list(range(x[0],x[-1]+1))))
